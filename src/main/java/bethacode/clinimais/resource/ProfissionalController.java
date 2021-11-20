8bf54f16907e11de1d9e8f85ed857d8b6e0c5874
Alex Cardoso <jesus.alexcardoso@gmail.com>
2021-11-20 11:30:58 -0300

    Criada API dos profissionais;

--- src/main/java/bethacode/clinimais/resource/ProfissionalController.java
+++ src/main/java/bethacode/clinimais/resource/ProfissionalController.java
@@ -1,15 +1,15 @@
 package bethacode.clinimais.resource;
 
-import bethacode.clinimais.model.Paciente;
 import bethacode.clinimais.model.Profissional;
-import bethacode.clinimais.repository.PacienteRepository;
 import bethacode.clinimais.repository.ProfissionalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +30,37 @@ public class ProfissionalController {
     @PostMapping
     public Profissional create(@Valid @RequestBody Profissional profissional){
         return repository.save(profissional);
+    }
+
+    @GetMapping("/{id}")
+    public ProfissionalDTO getProfissionalId(@PathVariable(value = "id") Long id) throws EntityNotFoundException {
+
+        Profissional profissionalFind = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Profissional não encontrado com ID :: " + id));
+
+        return ProfissionalDTO.toDTO(profissionalFind);
+    }
+
+    @PutMapping("/{id}")
+    public Profissional update(@PathVariable(value = "id") Long id,
+                           @RequestBody Profissional profissional) throws EntityNotFoundException {
+        Profissional profissionalFind = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Profissional não encontrado com ID: " + id));
+
+        profissionalFind.setNome(profissional.getNome());
+        profissionalFind.setTipoProfissional(profissional.getTipoProfissional());
+
+        return repository.save(profissionalFind);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Long id) throws EntityNotFoundException {
+        Profissional profissionalFind = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Profissional não encontrado com ID :: " + id));
+
+        repository.delete(profissionalFind);
+
+        return ResponseEntity.noContent().build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
--- src/main/java/bethacode/clinimais/resource/ProfissionalDTO.java
+++ src/main/java/bethacode/clinimais/resource/ProfissionalDTO.java
@@ -1,9 +1,53 @@
 package bethacode.clinimais.resource;
 
+import bethacode.clinimais.model.Paciente;
+import bethacode.clinimais.model.Profissional;
 import bethacode.clinimais.model.TipoProfissional;
 
 public class ProfissionalDTO {
-    //private String
+    private Long id;
+    private String nome;
     private TipoProfissional tipoProfissional;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoProfissional getTipoProfissional() {
+        return tipoProfissional;
+    }
+
+    public void setTipoProfissional(TipoProfissional tipoProfissional) {
+        this.tipoProfissional = tipoProfissional;
+    }
+
+    public static ProfissionalDTO toDTO(Profissional profissional){
+        ProfissionalDTO dto = new ProfissionalDTO();
+        dto.setId(profissional.getId());
+        dto.setNome(profissional.getNome());
+        dto.setTipoProfissional(profissional.getTipoProfissional());
+
+        return dto;
+    }
+
+    public static Profissional fromDTO(ProfissionalDTO dto){
+        Profissional entity = new Profissional();
+        entity.setId(dto.getId());
+        entity.setNome(dto.getNome());
+        entity.setTipoProfissional(dto.getTipoProfissional());
+
+        return entity;
+    }
 }
